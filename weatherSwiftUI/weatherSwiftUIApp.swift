54e68158919d83f32c10859ef22427d3a78d7c40
Rachel Mroz <mroz.rachelj@gmail.com>
2022-07-13 13:40:09 -0500

    Initial Commit

---
+++ weatherSwiftUI/weatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherSwiftUIApp.swift
+//  weatherSwiftUI
+//
+//  Created by rmroz on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
